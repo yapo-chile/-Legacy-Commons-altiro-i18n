ac26b6db604e1acfbda15a9e836d0e25e244f792
Claudio Donoso <claudio.donoso@schibsted.cl>
2018-01-25 15:25:59 -0300

    update(master): update es language, values ​​are reordered

--- i18n/es.js
+++ i18n/es.js
@@ -1,36 +1,32 @@
 /* eslint-disable import/prefer-default-export */
 export const i18nEs = {
-  bannerSMS: {
-    deeplink: {
-      android: 'https://yapo.app.link/smsinfobip',
-      ios: 'https://yapo.app.link/smsinfobip',
+  BANNER_SMS: {
+    DEEPLINKS: {
+      ANDROID: 'https://yapo.app.link/smsinfobip',
+      IOS: 'https://yapo.app.link/smsinfobip',
     },
-    options: {
-      ios: {
-        name: 'IOS',
+    OPTIONS: {
+      IOS: {
+        NAME: 'IOS',
       },
-      android: {
-        name: 'Android',
+      ANDROID: {
+        NAME: 'Android',
       },
     },
-    optionSelected: 'android',
-    response: {
-      error: 'Error de conexión. Intente nuevamente.',
-      invalid: 'Número inválido',
-      success: 'Ahora recibirás el enlace para descargar nuestra app',
-      sending: 'Enviando...',
-    },
-    text: {
-      description: 'Ingresa tu <b class="bannerSMSTextBold">número de teléfono</b> y te enviaremos un enlace para descargar la app',
-      from: 'Yapo.cl',
-      message: 'Descarga la App de Yapo aquí',
-      buttonText: 'Enviar',
-    },
-    title: 'Compra y vende aún más rápido con la app de',
-    url: '/sms/send_sms',
+    OPTION_SELECTED: 'android',
+    DESCRIPTION: 'Ingresa tu <b class="bannerSMSTextBold">número de teléfono</b> y te enviaremos un enlace para descargar la app',
+    SUCCESS_MSG: 'Ahora recibirás el enlace para descargar nuestra app',
+    TITLE: 'Compra y vende aún más rápido con la app de',
+    URL: '/sms/send_sms',
   },
-  favButton: {
-    empty: 'Agregar a favoritos:',
-    selected: 'Agregado a favoritos:',
+  ADD_TO_FAVORITES: 'Agregar a favoritos:',
+  ADDED_TO_FAVORITES: 'Agregado a favoritos:',
+  DOWNLOAD_APP_MOVIL_YAPO_HERE: 'Descarga la App de Yapo aquí',
+  SEND: 'Enviar',
+  SENDING: 'Enviando...',
+  YAPO_URL: 'Yapo.cl',
+  ERRORS : {
+    CONNECTION: 'Error de conexión. Intente nuevamente.',
+    INVALID_NUMBER: 'Número inválido',
   },
 };
