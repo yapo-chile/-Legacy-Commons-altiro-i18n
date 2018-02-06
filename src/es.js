ada02d05689969e871ec626a885166181c28075b
Miguel <miguel.mora@schibsted.cl>
2018-02-06 10:46:06 -0300

    cambio minúscula a mayúscula
    cambio minúscula a mayúscula

--- src/es.js
+++ src/es.js
@@ -32,7 +32,7 @@ export const es = {
   BUILDERS: {
     PUBLISHED_AT: 'Publicado',
     SINCE: 'Desde',
-    TO:'hasta',
+    TO:'Hasta',
     PROJECT: 'Proyecto',
     INFORMATION: 'Información',
     RENT: 'Arriendo',
