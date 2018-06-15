eb296571aa08a2d087fffd043e3e90ecc5121063
Miguel <miguel.mora@schibsted.cl>
2018-06-15 18:30:59 -0400

    add pd words

--- src/payment-delivery.js
+++ src/payment-delivery.js
@@ -11,6 +11,22 @@ const DICTIONARY = {
   THANKS_TO_TRUST_YAPO: 'Gracias por confiar en Yapo.cl para hacer un buen negocio, miles de personas ya están comprando y vendiendo.',
   TRANSACTION_QUESTION: '¿Dónde te depositamos?',
   TRANSACTION_DATA: 'También necesitamos tus datos para el retiro del producto.',
+  REJECT_BUY_TITLE: '¡Oops la transacción no pudo ser realizada!',
+  REJECT_BUY_SUBTITLE: 'Parece que hubo un problema con tu compra y no pudo completarse el pago.',
+  REJECT_BUY_BUTTON: 'Volver a intentar',
+  SUCCESSFUL_BUY_TITLE: '¡Felicitaciones!',
+  SUCCESSFUL_BUY_SUBTITLE: 'Tu compra se realizó exitosamente',
+  SUCCESSFUL_BUY_CONTENT: 'Recibirás un correo con tu boleta electrónica confirmando tu compra.',
+  POST_EVAL_POSITIVE_TITLE: '¡Gracias por utilizar Pago y despacho!',
+  POST_EVAL_POSITIVE_SUBTITLE: 'Gracias por confiar en Yapo.cl para hacer un buen negocio, miles de personas ya están comprando y vendiendo.',
+  POST_EVAL_POSITIVE_BUTTON: 'Ver avisos',
+  POST_EVAL_NEGATIVE_TITLE: '¡Lamentamos esta situación!',
+  POST_EVAL_NEGATIVE_SUBTITLE: 'Nos pondremos en contacto con el vendedor para solucionarlo, te enviaremos un correo con las intrucciones en caso de reembolso.',
+  REJECT_SALE_TITLE: '¡Has rechazado la venta!',
+  REJECT_SALE_SUBTITLE: 'Nos pondremos en contacto con el comprador para el reembolso. Esperamos que la próxima venta sea exitosa.',
+  POST_ACCEPT_BUY_TITTLE: '¡Gracias por utilizar Pago y despacho!',
+  POST_ACCEPT_BUY_SUBTITTLE: 'Gracias por confiar en Yapo.cl para hacer un buen negocio, miles de personas ya están comprando y vendiendo.',
+  POST_ACCEPT_BUY_BUTTON: 'Ver avisos',
 };
 
 export default DICTIONARY;
