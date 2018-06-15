078fe41631ab7069bbf5402015960254b42bbea8
Miguel <miguel.mora@schibsted.cl>
2018-06-15 10:43:04 -0400

    add payment-delivery words

--- src/payment-delivery.js
+++ src/payment-delivery.js
@@ -5,6 +5,8 @@ const DICTIONARY = {
   READY_TO_USE: '¿Listo para utilizar Lemonpay?',
   TRANSACTION_QUESTION: '¿Dónde te depositamos?',
   TRANSACTION_DATA: 'También necesitamos tus datos para el retiro del producto.',
+  MORE_INFO_ABOUT_PD: 'Más información sobre Pago y despacho',
+  PD_DESCRIPTION_START: 'Para empezar necesitamos que completes la información relacionada al envío del producto. Recuerda que el precio debe haber sido acordado previamente con el comprador, para que este no reciba cargos de devolución.',
 };
 
 export default DICTIONARY;
