536f92c11e798179e3a9fa2576fd7bdd08e84ebf
Felipe Egas <felipe.egas@schibsted.cl>
2018-07-25 17:35:13 -0400

    recurring payment words

--- src/recurring-payment.js
+++ src/recurring-payment.js
@@ -5,6 +5,14 @@ const DICTIONARY = {
   SECOND_CARD_TITLE: 'PACK incluido:',
   INFO_TEXT: 'Para continuar debes aceptar las condiciones del servicio.',
   CONFIRM_TEXT: 'Conozco y estoy de acuerdo con las condiciones de este servicio.',
+  FAIL_PAGE_TITLE: 'Oops! No se pudo suscribir el Pack',
+  FAIL_PAGE_DESCRIPTION: 'Tuvimos un problema al suscribir tu nuevo Pack, por favor reintenta en unos minutos. \n\n Si el fallo persiste ponte <b>en contacto con tu ejecutivo.</b>',
+  SUCCESS_PAGE_DESCRIPTION: 'El primer cobro y activación de tu Pack se realizará en los próximos minutos. Luego se renovará <b>automáticamente cada mes.</b>',
+  SUCCESS_PAGE_TITLE: '¡La suscripción de tu Pack fue exitosa!',
+  WARNING_PAGE_DESCRIPTION: 'Si deseas suscribir otro Pack a tu cuenta, por favor ponte <b>en contacto con tu ejecutivo.</b>',
+  WARNING_PAGE_TITLE: 'No hay suscripciones de Packs pendientes',
+  SUBSCRIPTION_DATE: 'Fecha suscripción',
+  PACK_QUOTAS: 'Cupos del Pack',
 };
 
 export default DICTIONARY;
