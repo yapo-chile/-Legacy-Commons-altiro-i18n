7d79a0bb869d991205b1f6bf458efea59b9be5be
MiguelMora <miguelmora@miguelmora.local>
2019-02-25 14:47:00 -0300

    added common section

--- src/es.js
+++ src/es.js
@@ -20,13 +20,15 @@ export const es = {
       },
     },
     OPTION_SELECTED: 'android',
-    DESCRIPTION: 'Ingresa tu <b class="bannerSMSTextBold">número de teléfono</b> y te enviaremos un enlace para descargar la app',
+    DESCRIPTION:
+      'Ingresa tu <b class="bannerSMSTextBold">número de teléfono</b> y te enviaremos un enlace para descargar la app',
     SUCCESS_MSG: 'Ahora recibirás el enlace para descargar nuestra app',
     TITLE: 'Compra y vende aún más rápido con la app de',
     URL: '/sms/send_sms',
   },
   ACCEPT_REQUEST: 'Aceptar la solicitud',
-  ACCEPT_REQUEST_EXPLAIN: 'Si estás conforme con vender este producto a ese usuario, será necesario un par de datos para el pago y envío.',
+  ACCEPT_REQUEST_EXPLAIN:
+    'Si estás conforme con vender este producto a ese usuario, será necesario un par de datos para el pago y envío.',
   ADD_TO_FAVORITES: 'Agregar a favoritos:',
   ADDED_TO_FAVORITES: 'Agregado a favoritos:',
   ADDRESS: 'Dirección',
@@ -34,7 +36,8 @@ export const es = {
   BUY: 'Comprar',
   BUY_AND_PAY_SECURE: 'Compra y vende de forma segura sin moverte de tu casa.',
   BUY_PRODUCT: 'Compra el producto',
-  BUY_PRODUCT_EXPLAIN: 'Ingresa tus datos de contacto y domicilio requeridos para la entrega del producto. Realiza tu pago online.',
+  BUY_PRODUCT_EXPLAIN:
+    'Ingresa tus datos de contacto y domicilio requeridos para la entrega del producto. Realiza tu pago online.',
   BACK: 'Volver',
   BACK_TO_YAPO: 'Volver a Yapo.cl',
   EMAIL: 'E-mail',
@@ -46,17 +49,21 @@ export const es = {
   COMMUNES: 'Comunas',
   COMMERCIAL_DETAIL: 'Detalle comercial',
   DELIVERY_TERMS: 'Plazos de entrega',
-  DELIVERY_TERMS_EXPLAIN: 'Una vez aceptada la compra y retirado el producto, la entrega se realizará dentro de los próximos días.',
+  DELIVERY_TERMS_EXPLAIN:
+    'Una vez aceptada la compra y retirado el producto, la entrega se realizará dentro de los próximos días.',
   DOWNLOAD_APP_MOVIL_YAPO_HERE: 'Descarga la App de Yapo aquí',
   DNI: 'RUT',
   FASTER_SALES: 'Ventas más rápidas',
-  FASTER_SALES_EXPLAIN: 'Ahora puedes vender lejos de ti sin riesgos, tus potenciales compradores se multiplican, lo que acelera tus ventas.',
+  FASTER_SALES_EXPLAIN:
+    'Ahora puedes vender lejos de ti sin riesgos, tus potenciales compradores se multiplican, lo que acelera tus ventas.',
   FREE_SHIPPING: '¡Envío gratis!',
-  FREE_SHIPPING_EXPLAIN: 'Sin cobros de envío para el vendedor, no tienes que adelantar nada, y además te recogen el paquete en casa.',
+  FREE_SHIPPING_EXPLAIN:
+    'Sin cobros de envío para el vendedor, no tienes que adelantar nada, y además te recogen el paquete en casa.',
   GREETINGS: 'Felicidades por haber realizado una venta a través de Pago y despacho.',
   GO_TO_MY_ADS: 'Ir a mis avisos',
   GUARANTY_DEVOLUTION: 'Garantía de devolución',
-  GUARANTY_DEVOLUTION_EXPLAIN: 'Si el envío no llega o no es lo que compraste, te devolvemos el dinero y nos hacemos cargo del envío de vuelta.',
+  GUARANTY_DEVOLUTION_EXPLAIN:
+    'Si el envío no llega o no es lo que compraste, te devolvemos el dinero y nos hacemos cargo del envío de vuelta.',
   HOW_WORK: '¿Cómo funciona?',
   HOW_WORKS: 'Cómo funciona',
   MOVIL_PHONE: 'Celular',
@@ -65,30 +72,40 @@ export const es = {
   ORIGINAL_PRICE: 'Precio original',
   PAID_PRICE: 'Precio pagado',
   PAY_AND_RECEPTION: 'Pago y recepción',
-  PAY_AND_RECEPTION_EXPLAIN: 'Tu pago será retenido hasta que al recibir el producto te asegures que está en las condiciones prometidas.',
+  PAY_AND_RECEPTION_EXPLAIN:
+    'Tu pago será retenido hasta que al recibir el producto te asegures que está en las condiciones prometidas.',
   PAYMENT_DELIVERY_ADVANTAGE: 'Ventajas de Pago y despacho',
   PAYMENT_DELIVERY_ADVANTAGE_MIN: 'Ventajas de pago y despacho',
-  PAYMENT_DELIVERY_INFO: '* Servicio de Pago y despacho disponible solo en la provincia de Santiago, Región Metropolitana. Próximamente en otras regiones del país.',
-  PAYMENT_DELIVERY_INFO_REMEMBER: 'Recuerda que el precio debe haber sido acordado previamente con el vendedor, para no recibir cargos de devolución',
-  PAYMENT_DELIVERY_INFO_BEGIN: 'Recuerda que el precio debe haber sido acordado previamente con el vendedor, para no recibir cargos de devolución',
+  PAYMENT_DELIVERY_INFO:
+    '* Servicio de Pago y despacho disponible solo en la provincia de Santiago, Región Metropolitana. Próximamente en otras regiones del país.',
+  PAYMENT_DELIVERY_INFO_REMEMBER:
+    'Recuerda que el precio debe haber sido acordado previamente con el vendedor, para no recibir cargos de devolución',
+  PAYMENT_DELIVERY_INFO_BEGIN:
+    'Recuerda que el precio debe haber sido acordado previamente con el vendedor, para no recibir cargos de devolución',
   PAYMENT_DELIVERY_WELCOME: '¡Bienvenido a pago y despacho!',
   PAYMENT_TO_SELLER: 'Pago al vendedor',
-  PAYMENT_TO_SELLER_EXPLAIN: 'Cuando verifiques y apruebes el estado del producto se liberará el proceso de pago al vendedor.',
+  PAYMENT_TO_SELLER_EXPLAIN:
+    'Cuando verifiques y apruebes el estado del producto se liberará el proceso de pago al vendedor.',
   PRODUCT_DELIVERY: 'Entrega del producto',
-  PRODUCT_DELIVERY_EXPLAIN: 'Al aceptar la solicitud de compra tendrás que coordinar la entrega de tu producto en la dirección que desees.',
+  PRODUCT_DELIVERY_EXPLAIN:
+    'Al aceptar la solicitud de compra tendrás que coordinar la entrega de tu producto en la dirección que desees.',
   PAYMENT_RECEPTION: 'Recepción del pago',
-  PAYMENT_RECEPTION_EXPLAIN: 'Si tu producto fue aceptado por el comprador, el dinero retenido en forma segura será transferido a tu cuenta.',
+  PAYMENT_RECEPTION_EXPLAIN:
+    'Si tu producto fue aceptado por el comprador, el dinero retenido en forma segura será transferido a tu cuenta.',
   REJECT: 'Rechazar',
   RETURN: 'Devolución',
-  RETURN_EXPLAIN: 'Si el comprador no está conforme con el producto, te lo llevamos de vuelta sin costo para tí.',
+  RETURN_EXPLAIN:
+    'Si el comprador no está conforme con el producto, te lo llevamos de vuelta sin costo para tí.',
   RETURN_GUARANTEE: 'Garantía de devolución',
-  RETURN_GUARANTEE_EXPLAIN: 'Antes de que envíes, ya tenemos el dinero del comprador retenido como garantía de pago.',
+  RETURN_GUARANTEE_EXPLAIN:
+    'Antes de que envíes, ya tenemos el dinero del comprador retenido como garantía de pago.',
   WE_GO_TO_YOUR_HOME: 'Vamos hasta la puerta de tu casa para garantizar tu mayor comodidad',
   PERSONAL_DATA: 'Datos personales',
   PRICE: 'Precio',
   REGION: 'Región',
   RETRY: 'Reintentar',
-  SAVE_YOUR_MONEY: 'Guardamos tu dinero en un depósito seguro y no lo transferimos al vendedor hasta que compruebes tu producto.',
+  SAVE_YOUR_MONEY:
+    'Guardamos tu dinero en un depósito seguro y no lo transferimos al vendedor hasta que compruebes tu producto.',
   SECURE_PAY: 'Pago Seguro',
   SECURE_BUY: 'Compra Segura',
   SEND: 'Enviar',
@@ -99,11 +116,13 @@ export const es = {
   SOMEONE_BOUGHT_YOUR_PRODUCT: 'pagó por tu producto',
   SUMMARY: 'Resumen de tu compra',
   TO_YOUR_HOME_DOOR: 'A la puerta de tu casa',
-  TO_YOUR_HOME_DOOR_EXPLAIN: 'Nos encargamos de retirar el producto y entregartelo en la dirección que más te acomode.',
+  TO_YOUR_HOME_DOOR_EXPLAIN:
+    'Nos encargamos de retirar el producto y entregartelo en la dirección que más te acomode.',
   TOTAL_PRICE: 'Precio Total',
   WHERE_IS_PRODUCT_SENT: '¿Dónde te enviamos el producto?',
   WHAT_IS_PAYMENT_DELIVERY: '¿Qué es pago y despacho?',
-  WHAT_IS_PAYMENT_DELIVERY_EXPLAIN: 'Es el nuevo servicio de pago y despacho seguro de Yapo.cl en conjunto con Lemonpay y Blue Express nos encargaremos de cuidar tu dinero y del envío de tu producto llevándolo donde tú nos digas. Te acompañaremos en la negociación, pago, envío y cobro.',
+  WHAT_IS_PAYMENT_DELIVERY_EXPLAIN:
+    'Es el nuevo servicio de pago y despacho seguro de Yapo.cl en conjunto con Lemonpay y Blue Express nos encargaremos de cuidar tu dinero y del envío de tu producto llevándolo donde tú nos digas. Te acompañaremos en la negociación, pago, envío y cobro.',
   YAPO_URL: 'Yapo.cl',
   ERRORS: {
     CONNECTION: 'Error de conexión. Intente nuevamente.',
@@ -193,6 +212,13 @@ export const es = {
       MESSAGE: 'Mensaje',
       APPROVED_CREDIT: 'Tienes crédito aprobado',
     },
+  },
+  COMMON: {
+    FOLLOW_US: 'Síguenos en:',
+    CONTACT_US: 'Contáctanos',
+    OUR_RULES: 'Lee nuestras reglas',
+    COPYRIGHT: ' Yapo.cl SpA',
+    ANY_QUESTIONS: '¿Tienes alguna duda?',
   },
   MESSAGING_CENTER,
   RECURRING_PAYMENT,
