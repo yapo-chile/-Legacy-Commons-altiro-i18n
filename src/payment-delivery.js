76ed8d81cb4bd2786c0217743bb1484dffe7f246
MiguelMora <miguelmora@miguelmora.local>
2019-02-26 11:15:18 -0300

    change READY_TO_USE value

--- src/payment-delivery.js
+++ src/payment-delivery.js
@@ -12,7 +12,7 @@ const DICTIONARY = {
   HOW_TO_USE: 'Para empezar necesitamos que completes la información relacionada al envío del producto. Recuerda que el precio que ingreses debe haber sido acordado previamente con el vendedor, para no recibir cargos de devolución.',
   LET_US_KNOW_RECEPTION: 'Nos interesa saber cual es la razón por la cual quieres rechazar la compra del producto. Selecciona cual de las siguientes razones fue la que te hizo rechazar el producto',
   LET_US_KNOW_REJECT: 'Nos interesa saber cuál es la razón por la que quiere rechazar la venta de este producto. Por favor, selecciona cuál de los siguientes motivos fue el que te hizo rechazar la venta',
-  READY_TO_USE: '¿Listo para utilizar Lemonpay?',
+  READY_TO_USE: '¿Listo para utilizar Pago y Despacho?',
   MORE_INFO_ABOUT_PD: 'Más información sobre Pago y despacho',
   PD_DESCRIPTION_START: 'Para empezar necesitamos que completes la información relacionada al envío del producto. Recuerda que el precio debe haber sido acordado previamente con el comprador, para que este no reciba cargos de devolución.',
   POSSIBLE_REASONS: 'Posibles razones',
